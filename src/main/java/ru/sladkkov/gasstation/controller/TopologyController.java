15d11e3f3279a1dd82b7e8697770a7e569371028
eanpilogov <eanpilogov@axbit.ru>
2022-12-23 21:48:49 +0400

    govno3

--- src/main/java/ru/sladkkov/gasstation/controller/TopologyController.java
+++ src/main/java/ru/sladkkov/gasstation/controller/TopologyController.java
@@ -34,16 +34,16 @@ public class TopologyController {
     @PostConstruct
 
     public ResponseEntity<List<String>> topologyCreateController() {//@RequestBody TopologyDto topologyDTO) {
-
+//todo parser mainToplogy[][] and serviceTopogy[][]
         TopologyDto topologyDTO = new TopologyDto();
         topologyDTO.setTopology(new int[10][10]);
         topologyDTO.setTopologyLength(10);
         topologyDTO.setTopologyWidth(10);
         topologyDTO.setTopologyServiceLength(0);
-        int enter = 0;
-        int exit = 0;
-        int trk = 0;
-        int cashBox = 0;
+        int enter = 0;//1
+        int exit = 0;//2
+        int trk = 0;//3
+        int cashBox = 0;//4
         List<Integer> trkArrayX = new ArrayList<>();
         List<Integer> trkArrayY = new ArrayList<>();
         int startX = 0;
@@ -186,12 +186,146 @@ public class TopologyController {
             waveCopy = null;
         }
         System.out.println(raceList);
+        serviceTopology(topology);
         return ResponseEntity.ok(raceList);
     }
 
-    @GetMapping("/parse/xml/{length}/{width}")
-    public ResponseEntity<String> parseXmlToMassiveObject(@RequestBody String xmlFile, @PathVariable int length, @PathVariable int width) throws IOException {
-        TopologyElement[][] topologyElements = xmlParser.parseHashMapToMassiveObject(xmlParser.parseXmlToHashMapOfIdAndTypeObject(xmlFile), length, width);
-        return ResponseEntity.ok(Arrays.deepToString(topologyElements));
+    public List<String> serviceTopology( int[][] topology) {
+
+        int baks = 0;
+        ArrayList<Integer> bakArrayX = new ArrayList<>();
+        ArrayList<Integer> bakArrayY = new ArrayList<>();
+        int startX=0;
+        int startY=0;
+        for (int i = 0; i < topology.length; i++) {
+            int[] ints = topology[i];
+            for (int j = 0; j < ints.length; j++) {
+                int anInt = ints[j];
+                if (anInt == 1) {
+                    Preconditions.checkState(false, "Въезд не должен находится в служебной зоне");
+                }
+                if (anInt == 2) {
+                    Preconditions.checkState(false, "Выезд не должен находится в служебной зоне");
+                }
+                if (anInt == 3) {
+                    Preconditions.checkState(false, "ТРК не должна находится в служебной зоне");
+                }
+                if (anInt == 7) {
+                 startX=i;
+                 startY=j;
+                }
+                if (anInt == 4)
+                    Preconditions.checkState(false, "Касса не должна находится в служебной зоне");
+                if (anInt == 5) {
+                    baks++;
+                    bakArrayX.add(i);
+                    bakArrayY.add(j);
+
+                    if (j == 0 && i == 0) {
+                        Preconditions.checkState((topology[i + 1][j + 1] == 0) &&
+                                        (topology[i + 1][j] == 0) &&
+                                        (topology[i][j + 1] == 0),
+                                "Элементы топологии должны располагаться как минимум в одной клетке друг от друга");
+                    } else {
+                        if (j == 0) {
+                            Preconditions.checkState((topology[i + 1][j + 1] == 0) &&
+                                            (topology[i + 1][j] == 0) &&
+                                            (topology[i][j + 1] == 0) &&
+                                            (topology[i - 1][j + 1] == 0) &&
+                                            (topology[i - 1][j] == 0),
+                                    "Элементы топологии должны располагаться как минимум в одной клетке друг от друга");
+
+                        } else {
+                            if (i == 0) {
+
+                                Preconditions.checkState((topology[i + 1][j + 1] == 0) &&
+                                        (topology[i + 1][j] == 0) &&
+                                        (topology[i + 1][j - 1] == 0) &&
+                                        (topology[i][j + 1] == 0) &&
+                                        (topology[i][j - 1] == 0), "Элементы топологии должны располагаться как минимум в одной клетке друг от друга");
+                            } else if (i == topology.length - 1 && j == topology[i].length - 1) {
+                                Preconditions.checkState(
+                                        (topology[i][j - 1] == 0) &&
+                                                (topology[i - 1][j] == 0) &&
+                                                (topology[i - 1][j - 1] == 0),
+                                        "Элементы топологии должны располагаться как минимум в одной клетке друг от друга");
+
+                            } else {
+                                if (i == topology.length - 1) {
+                                    Preconditions.checkState(
+                                            (topology[i][j + 1] == 0) &&
+                                                    (topology[i][j - 1] == 0) &&
+                                                    (topology[i - 1][j + 1] == 0) &&
+                                                    (topology[i - 1][j] == 0) &&
+                                                    (topology[i - 1][j - 1] == 0),
+                                            "Элементы топологии должны располагаться как минимум в одной клетке друг от друга");
+                                } else {
+                                    if (j == topology[i].length - 1) {
+                                        Preconditions.checkState(
+                                                (topology[i + 1][j] == 0) &&
+                                                        (topology[i + 1][j - 1] == 0) &&
+                                                        (topology[i][j - 1] == 0) &&
+                                                        (topology[i - 1][j] == 0) &&
+                                                        (topology[i - 1][j - 1] == 0),
+                                                "Элементы топологии должны располагаться как минимум в одной клетке друг от друга");
+                                    } else {
+                                        Preconditions.checkState((topology[i + 1][j + 1] == 0) &&
+                                                        (topology[i + 1][j] == 0) &&
+                                                        (topology[i + 1][j - 1] == 0) &&
+                                                        (topology[i][j + 1] == 0) &&
+                                                        (topology[i][j - 1] == 0) &&
+                                                        (topology[i - 1][j + 1] == 0) &&
+                                                        (topology[i - 1][j] == 0) &&
+                                                        (topology[i - 1][j - 1] == 0),
+                                                "Элементы топологии должны располагаться как минимум в одной клетке друг от друга");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    System.out.println(i + "  " + j);
+
+                }
+            }
+
+
+        }
+        Preconditions.checkState(baks < 7 && baks > 0, "Колличество баков должно быть от 0 до 7");
+        String race;
+        List<String> raceList = new ArrayList<>();
+        for (int k = 0; k < bakArrayX.size(); k++) {
+            WaveAlg waveCopy = new WaveAlg(topology.length, topology[0].length);
+            for (int i = 0; i < topology.length; i++) {
+                int[] ints = topology[i];
+                for (int j = 0; j < ints.length; j++) {
+                    int anInt = ints[j];
+                    if (anInt != 0 && anInt != 1 && anInt != 2 && (anInt != bakArrayY.get(k) && j != bakArrayY.get(k))) {
+                        waveCopy.block(i, j);
+                    }
+                }
+            }
+            race = waveCopy.findPath(startX + 1, startY + 1, bakArrayX.get(k), bakArrayY.get(k));//сверху
+            race += waveCopy.findPath(bakArrayX.get(k), bakArrayY.get(k), startX + 1, startY + 1);//сверху
+            race += (startX + " " + startY);
+            waveCopy.traceOut();
+            race = race.replace("][", " ,");
+            race = race.replace("]", ", ");
+            raceList.add(race);
+            System.out.println(race);
+            waveCopy = null;
+        }
+        System.out.println(raceList);
+        return raceList;
     }
-}
+
+
+
+
+
+
+@GetMapping("/parse/xml/{length}/{width}")
+public ResponseEntity<String> parseXmlToMassiveObject(@RequestBody String xmlFile,@PathVariable int length,@PathVariable int width)throws IOException{
+        TopologyElement[][]topologyElements=xmlParser.parseHashMapToMassiveObject(xmlParser.parseXmlToHashMapOfIdAndTypeObject(xmlFile),length,width);
+        return ResponseEntity.ok(Arrays.deepToString(topologyElements));
+        }
+        }
