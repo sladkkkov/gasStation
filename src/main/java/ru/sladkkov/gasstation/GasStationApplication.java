f4367709d9357c1986438c1f9a0530cce5356a6f
eanpilogov <eanpilogov@axbit.ru>
2022-12-28 01:41:54 +0400

    govno6

--- src/main/java/ru/sladkkov/gasstation/GasStationApplication.java
+++ src/main/java/ru/sladkkov/gasstation/GasStationApplication.java
@@ -2,9 +2,11 @@ package ru.sladkkov.gasstation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 //@EnableWebSecurity
+@EnableJpaRepositories
 public class GasStationApplication {
 
     public static void main(String[] args) {
--- src/main/java/ru/sladkkov/gasstation/model/Topology.java
+++ src/main/java/ru/sladkkov/gasstation/model/Topology.java
@@ -2,11 +2,13 @@ package ru.sladkkov.gasstation.model;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 
 @Data
+@Entity
 @Table(name = "topology")
 public class Topology {
     @Id
