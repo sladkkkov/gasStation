fed04d728a48217e1020c236cdb16008cf1d53ca
sladkkov <sladkkov@yandex.ru>
2022-12-29 01:32:10 +0400

    fix

--- src/main/java/ru/sladkkov/gasstation/model/Topology.java
+++ src/main/java/ru/sladkkov/gasstation/model/Topology.java
@@ -2,6 +2,7 @@ package ru.sladkkov.gasstation.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -13,6 +14,7 @@ import javax.persistence.Table;
 public class Topology {
     @Id
     protected Long id;
+    @Column(name = "xml_topology", length = 50000)
     private String xmlTopology;
     private Integer length;
     private Integer width;
